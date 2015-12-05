a88085b83d3dd58db7dac9c762a0257634f3edde
Владимир <v.grabko99@gmail.com>
2015-12-05 21:39:29 +0200

    Исправлен шаблонизатор. В шаблонизатор добавленна потдержка if и 4 уровня кеша

--- js/modules/test/test.js
+++ js/modules/test/test.js
@@ -1,20 +1,27 @@
 (function(_){var m = _.modules.name("test");if(m){
         
-        m.alert = function(){
+    m.alert = function(){
                 alert("test");
-        }
+    }
 
-        m.init = function(location, data){
-        		switch(location){
-        			case "click":
-        				console.info(location);
-        			break;
+    m.init = function(location, data){	
+       var start = new Date();
+        _.views.Get("/js/modules/test/json/ru.json", "/js/modules/test/html/test.html", {
+        	boll : true
+        }, "#message")
 
-        			case "test":
-        				console.info(data);
-        			break;
-        		}
-                
-        }
+        var end = new Date();
+        console.info('Скорость ' + (end.getTime()-start.getTime()) + ' мс');
+
+        start = new Date();
+        
+        _.views.Get("/js/modules/test/json/ru.json", "/js/modules/test/html/test.html", {
+        	boll : false
+        }, "#message")
+        
+        end = new Date();
+        console.info('Скорость ' + (end.getTime()-start.getTime()) + ' мс');
+
+    }
 
 }})(_);

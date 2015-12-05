78cc1fad6ba8f9a24e69f6cba21414d617ad6abd
Владимир <v.grabko99@gmail.com>
2015-12-05 23:09:41 +0200

    модуль test теперь полноценно обрабатывает урл. Показана песочница.

--- js/modules/test/test.js
+++ js/modules/test/test.js
@@ -1,24 +1,26 @@
 (function(_){var m = _.modules.name("test");if(m){
-        
-    m.alert = function(){
-                alert("test");
+    var views = _.expansion.load("views");   
+    var test = {
+    	alert : function(){
+        	alert("test");
+    	},
+    	clik : function(){
+    		views.Get("/js/modules/test/json/ru.json", "/js/modules/test/html/test.html", {
+        		boll : true
+        	}, "#message");
+    	}
     }
+    
+
 
     m.init = function(location, data){	
-       var views = _.expansion.load("views");
-       
+        if (test[location] != undefined){
+        	test[location](data);
+        } else {
+        	alert("404");
+        }
 
-        views.Get("/js/modules/test/json/ru.json", "/js/modules/test/html/test.html", {
-        	boll : true
-        }, "#message");
-
-        views.Get("/js/modules/test/json/ru.json", "/js/modules/test/html/test.html", {
-        	boll : false
-        }, "#message");
         
-
-
-
     }
 
 }})(_);
